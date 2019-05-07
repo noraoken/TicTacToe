31e6318a146f8c82bed9fb121c39f49619e20d60
noraoken <pandaxprest@gmail.com>
2019-05-07 00:14:42 -0400

    Add checkWinner and gameOver functions

--- script.js
+++ script.js
@@ -26,11 +26,35 @@ function startGame() {
 }
 
 function turnClick(square) {
-  turn(square.target.id, humanPlayer)
+  turn(square.target.id, humanPlayer);
 }
 
 // Place "O" on the square that human player clicks
 function turn(squareId, player) {
   originalBoard[squareId] = player;
   document.getElementById(squareId).innerText = player;
+  let gameWon = checkWinner(originalBoard, player);
+  if (gameWon) gameOver(gameWon);
+}
+
+function checkWinner(board, player) {
+  let plays = board.reduce((a, e, i) =>
+    (e === player) ? a.concat(i) : a, []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = {index: index, player: player};
+    }
+  }
+  return gameWon;
+}
+
+function gameOver(gameWon) {
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor = 
+      gameWon.player == humanPlayer ? "#60afe0" : "#e05b4a";
+  }
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", turnClick, false);
+  }
 }
